c5ddd704255c6daf2efc908aaf366dae59a236dc
neo-savage <sswave.5118@gmail.com>
2020-09-07 23:42:42 +0900

    fix:header

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { useWindowSize } from "react-use"
 import { HEADER_PATHS } from "@/constants/paths"
 import { Link } from "gatsby"
@@ -6,36 +6,46 @@ import MenuIcon from "@/assets/menu.svg"
 import { MobileMenu } from "@/components/MobileMenu"
 export const HeaderMenu = () => {
   const [isMobileMenuOpen, toggleMobileMenuOpen] = useState(false)
+  const [isMobile, toggleIsMobile] = useState(false)
   const { width } = useWindowSize()
-  if (width > 639)
-    return (
-      <nav className="list-none flex">
-        {/* 右端の要素はmargin rightなし */}
-        {HEADER_PATHS.map((path, i) => {
-          return HEADER_PATHS.length + 1 !== i ? (
-            <Link key={path.to} to={path.to} className="mr-2">
-              <li className="py-2 px-3 hover:text-teal-400">
-                {path.displayName}
-              </li>
-            </Link>
-          ) : (
-            <Link key={path.to} to={path.to}>
-              <li>{path.displayName}</li>
-            </Link>
-          )
-        })}
-      </nav>
-    )
-
+  useEffect(() => {
+    if (width < 640) {
+      toggleIsMobile(true)
+    }
+  }, [width])
   return (
     <>
-      <div
-        className="p-2 rounded shadow-lg"
-        onClick={() => toggleMobileMenuOpen(!isMobileMenuOpen)}
-      >
-        <MenuIcon className="w-6 h-6" />
-      </div>
-      <MobileMenu isOpen={isMobileMenuOpen} toggleOpen={toggleMobileMenuOpen} />
+      {isMobile ? (
+        <>
+          <div
+            className="p-2 rounded shadow-lg"
+            onClick={() => toggleMobileMenuOpen(!isMobileMenuOpen)}
+          >
+            <MenuIcon className="w-6 h-6" />
+          </div>
+          <MobileMenu
+            isOpen={isMobileMenuOpen}
+            toggleOpen={toggleMobileMenuOpen}
+          />
+        </>
+      ) : (
+        <nav className="list-none flex">
+          {/* 右端の要素はmargin rightなし */}
+          {HEADER_PATHS.map((path, i) => {
+            return HEADER_PATHS.length + 1 !== i ? (
+              <Link key={path.to} to={path.to} className="mr-2">
+                <li className="py-2 px-3 hover:text-teal-400">
+                  {path.displayName}
+                </li>
+              </Link>
+            ) : (
+              <Link key={path.to} to={path.to}>
+                <li>{path.displayName}</li>
+              </Link>
+            )
+          })}
+        </nav>
+      )}
     </>
   )
 }
